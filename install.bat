8a6a728cc4f27ba77a59254c9ad362009eacb8df
Tom Daquino <tom@havoc.sh>
2023-03-03 20:08:58 -0800

    Suppressing command outputs.

--- install.bat
+++ install.bat
@@ -2,37 +2,37 @@
 FOR /F "tokens=* USEBACKQ" %%F IN (`cd`) DO (SET cwd=%%F)
 
 echo "Downloading Python 3.10.10"
-c:\windows\system32\curl.exe -O -L https://www.python.org/ftp/python/3.10.10/python-3.10.10-embed-amd64.zip
+c:\windows\system32\curl.exe -O -L -s https://www.python.org/ftp/python/3.10.10/python-3.10.10-embed-amd64.zip
 
 echo "Extracting Python package contents"
 c:\windows\system32\windowspowershell\v1.0\powershell.exe Expand-Archive python-3.10.10-embed-amd64.zip
-move python-3.10.10-embed-amd64\* .
-move python-3.10.10-embed-amd64\Lib .
-move python-3.10.10-embed-amd64\Scripts .
-rmdir /S /Q python-3.10.10-embed-amd64
-del python-3.10.10-embed-amd64.zip
+move python-3.10.10-embed-amd64\* . >nul
+move python-3.10.10-embed-amd64\Lib . >nul
+move python-3.10.10-embed-amd64\Scripts . >nul
+rmdir /S /Q python-3.10.10-embed-amd64 >nul
+del python-3.10.10-embed-amd64.zip >nul
 
 echo "Downloading pip installer"
-c:\windows\system32\curl.exe -O -L https://bootstrap.pypa.io/get-pip.py
+c:\windows\system32\curl.exe -O -L -s https://bootstrap.pypa.io/get-pip.py
 
 echo "Running pip installer"
-python.exe get-pip.py --no-warn-script-location
+python.exe get-pip.py --no-warn-script-location >nul
 echo Lib\site-packages>> python310._pth
 
 echo "Installing havoc-remote requirements to embedded Python environment"
-python.exe -m pip install -r requirements.txt --no-warn-script-location
+python.exe -m pip install -r requirements.txt --no-warn-script-location >nul
 
 echo "Downloading havoc-pkg"
-c:\windows\system32\curl.exe -O -L https://github.com/havocsh/havoc-pkg/archive/refs/heads/endpoint_fix.zip
+c:\windows\system32\curl.exe -O -L -s https://github.com/havocsh/havoc-pkg/archive/refs/heads/endpoint_fix.zip
 
 echo "Extracting havoc-pkg contents"
 c:\windows\system32\windowspowershell\v1.0\powershell.exe Expand-Archive endpoint_fix.zip -DestinationPath havoc-pkg
 
 echo "Installing havoc-pkg to embedded Python environment"
-python.exe -m pip install havoc-pkg\havoc-pkg-endpoint_fix --no-warn-script-location
+python.exe -m pip install havoc-pkg\havoc-pkg-endpoint_fix --no-warn-script-location >nul
 
 echo "Downloading NSSM 2.24"
-c:\windows\system32\curl.exe -O -L https://nssm.cc/release/nssm-2.24.zip
+c:\windows\system32\curl.exe -O -L -s https://nssm.cc/release/nssm-2.24.zip
 
 echo "Extracting NSSM package contents"
 c:\windows\system32\windowspowershell\v1.0\powershell.exe Expand-Archive nssm-2.24.zip
@@ -53,4 +53,4 @@ echo "Starting HavocRemoteOperator service"
 nssm-2.24\nssm-2.24\win64\nssm.exe start HavocRemoteOperator
 
 echo "Install complete"
-timeout /t 15
+timeout /t 15 >nul
--- uninstall.bat
+++ uninstall.bat
@@ -3,20 +3,20 @@ echo "Stopping HavocRemoteOperator service"
 nssm-2.24\nssm-2.24\win64\nssm.exe stop HavocRemoteOperator
 
 echo "Uninstalling HavocRemoteOperator service"
-nssm-2.24\nssm-2.24\win64\nssm.exe remove HavocRemoteOperator confirm
+nssm-2.24\nssm-2.24\win64\nssm.exe remove HavocRemoteOperator confirm 
 
 echo "Backing up link.ini to link.ini.bak"
-copy link.ini link.ini.bak
+copy link.ini link.ini.bak >nul
 
 echo "Deleting all files and directories except the link.ini backup"
-rmdir /S /Q __pycache__
-rmdir /S /Q havoc-pkg
-rmdir /S /Q Lib
-rmdir /S /Q nssm-2.24
-rmdir /S /Q Scripts
+rmdir /S /Q __pycache__ >nul
+rmdir /S /Q havoc-pkg >nul
+rmdir /S /Q Lib >nul
+rmdir /S /Q nssm-2.24 >nul
+rmdir /S /Q Scripts >nul
 
 echo "Uninstall complete"
 echo "If you are not concerned with keeping the backup ini file, feel free to delete the havoc-remote-main directory."
-timeout /t 15
+timeout /t 15 >nul
 
-for /f %%F in ('dir /b /a-d ^| findstr /vile ".bak"') do del "%%F"
+for /f %%F in ('dir /b /a-d ^| findstr /vile ".bak"') do del "%%F" >nul
