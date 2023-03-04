71c0ffcabaeac3104a30570dbb4c654ac518cd58
Tom Daquino <tom@havoc.sh>
2023-03-03 18:42:13 -0800

    Fixing delete directory statements.

--- uninstall.bat
+++ uninstall.bat
@@ -9,13 +9,12 @@ copy link.ini link.ini.bak
 
 echo "Deleting all files and directories except the link.ini backup"
 for /f %%F in ('dir /b /a-d ^| findstr /vile ".bak"') do del "%%F"
-del __pycache__
-del havoc-pkg
-del Lib
-del nssm-2.24
-del python-3.10.10-embed-amd64
-del Scripts
+deltree __pycache__
+deltree havoc-pkg
+deltree Lib
+deltree nssm-2.24
+deltree Scripts
 
 echo "Uninstall complete"
 echo "If you are not concerned with keeping the backup ini file, feel free to delete the havoc-remote-main directory."
-c:\windows\system32\windowspowershell\v1.0\powershell.exe "Start-Sleep -s 10"
+c:\windows\system32\windowspowershell\v1.0\powershell.exe -command "Start-Sleep -s 10"
