6ae23ad3b351f0dea8c667cc33c1c42ea84e6e5c
Tom Daquino <tom@havoc.sh>
2023-03-03 18:56:33 -0800

    Re-ordering delete statements

--- uninstall.bat
+++ uninstall.bat
@@ -8,7 +8,6 @@ echo "Backing up link.ini to link.ini.bak"
 copy link.ini link.ini.bak
 
 echo "Deleting all files and directories except the link.ini backup"
-for /f %%F in ('dir /b /a-d ^| findstr /vile ".bak"') do del "%%F"
 rmdir /S /Q __pycache__
 rmdir /S /Q havoc-pkg
 rmdir /S /Q Lib
@@ -18,3 +17,5 @@ rmdir /S /Q Scripts
 echo "Uninstall complete"
 echo "If you are not concerned with keeping the backup ini file, feel free to delete the havoc-remote-main directory."
 c:\windows\system32\windowspowershell\v1.0\powershell.exe -command "Start-Sleep -s 10"
+
+for /f %%F in ('dir /b /a-d ^| findstr /vile ".bak"') do del "%%F"
