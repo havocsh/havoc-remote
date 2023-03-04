a612bb8c41ce46da7afbb2261423d0a39a6fa930
Tom Daquino <tom@havoc.sh>
2023-03-03 18:33:41 -0800

    Fixing deletes in uninstall.

--- uninstall.bat
+++ uninstall.bat
@@ -4,14 +4,17 @@ nssm-2.24\nssm-2.24\win64\nssm.exe stop HavocRemoteOperator
 echo "Uninstalling HavocRemoteOperator service"
 nssm-2.24\nssm-2.24\win64\nssm.exe remove HavocRemoteOperator confirm
 
-echo "Deleting nssm-2.24"
-del nssm-2.24
-
 echo "Backing up link.ini to link.ini.bak"
 copy link.ini link.ini.bak
 
-echo "Deleting all files except the link.ini backup"
-for /f %F in ('dir /b /a-d ^| findstr /vile ".bak"') do del "%F"
+echo "Deleting all files and directories except the link.ini backup"
+for /f %%F in ('dir /b /a-d ^| findstr /vile ".bak"') do del "%%F"
+del __pycache__
+del havoc-pkg
+del Lib
+del nssm-2.24
+del python-3.10.10-embed-amd64
+del Scripts
 
 echo "Uninstall complete"
 echo "If you are not concerned with keeping the backup ini file, feel free to delete the havoc-remote-main directory."
